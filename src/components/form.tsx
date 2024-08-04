53ec7d343bf7a5bf33740691531e020afeac7d6f
kjb2104 <kjblacklock@LAPTOP-NB47FNHH.localdomain>
2024-08-04 09:17:05 +0100

    add styling to error messages

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -24,164 +24,224 @@ const ProjectForm = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <br />
       <div className="mx-8">
-      <div className="space-y-12">
-        <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">
-            Submit a new Project
-          </h2>
-          <p className="mt-1 text-sm leading-6 text-gray-600">
-            This information will be displayed to other users.
-          </p>
-          <label className="block text-sm font-medium leading-6 text-gray-900">
-            Name of project:
-          </label>
-          <div className="mt-2">
-            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-              <input
-                placeholder="..."
-                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                {...register("Name", {
-                  required: true,
-                  maxLength: 50,
-                  minLength: 3,
-                })}
-              />
-<div>
+        <div className="space-y-12">
+          <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              Submit a new Project
+            </h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              This information will be displayed to other users.
+            </p>
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+              Name of project:
+            </label>
+            <div className="mt-2">
+              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <input
+                  placeholder="..."
+                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  {...register("Name", {
+                    required: true,
+                    maxLength: 50,
+                    minLength: 3,
+                  })}
+                />
+              </div>
+            </div>
+            {/* add styling to the validation errors which are triggered when the user leaves the input field */}
+            <div>
               {errors.Name?.type === "required" && (
                 <p
-                  className="block text-sm font-medium leading-6 text-red-600"
+                  className="pt-2.5 text-sm font-medium leading-6 text-red-600"
                   role="alert"
                 >
                   Project Name is required
                 </p>
               )}
               {errors.Name?.type === "maxLength" && (
-                <p role="alert"  className="block text-sm font-medium leading-6 text-red-600">Project Name must be under 50 characters</p>
+                <p
+                  role="alert"
+                  className="pt-2.5 text-sm font-medium leading-6 text-red-600"
+                >
+                  Project Name must be under 50 characters
+                </p>
               )}
               {errors.Name?.type === "minLength" && (
-                <p role="alert"  className="block text-sm font-medium leading-6 text-red-600">Project Name must be over 3 characters</p>
+                <p
+                  role="alert"
+                  className="pt 2.5 text-sm font-medium leading-6 text-red-600"
+                >
+                  Project Name must be over 3 characters
+                </p>
               )}
+            </div>
+            {/* add error handling to the form validation to inform the user what the specific issue is  */}
+            <br />
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+              Your name:
+            </label>
+            <div className="mt-2">
+              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <input
+                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  placeholder="...."
+                  {...register("Owner", {
+                    required: true,
+                    pattern: /^[A-Za-z- ]+$/gi,
+                  })}
+                />
               </div>
             </div>
-          </div>
-          {/* add error handling to the form validation to inform the user what the specific issue is  */}
-          <br />
-          <label className="block text-sm font-medium leading-6 text-gray-900">
-            Your name:
-          </label>
-          <div className="mt-2">
-            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-              <input
-                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                placeholder="...."
-                {...register("Owner", {
-                  required: true,
-                  pattern: /^[A-Za-z- ]+$/gi,
-                })}
-              />
+            <div>
               {errors.Owner?.type === "required" && (
-                <p role="alert">Your name is required</p>
+                <p
+                  className="pt-2.5 text-sm font-medium leading-6 text-red-600"
+                  role="alert"
+                >
+                  Your name is required
+                </p>
               )}
               {errors.Owner?.type === "pattern" && (
-                <p role="alert">Must only include letters</p>
+                <p
+                  className="pt-2.5 text-sm font-medium leading-6 text-red-600"
+                  role="alert"
+                >
+                  Must only include letters
+                </p>
               )}
-            
             </div>
-          </div>
-          <br />
-          <label className="block text-sm font-medium leading-6 text-gray-900">
-            Description of project:
-          </label>
-          <br />
-          <div className="mt-2">
-            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-              <textarea
-                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                placeholder="...."
-                {...register("Description", {
-                  required: true,
-                  maxLength: 200,
-                  minLength: 30,
-                })}
-              />
 
+            <br />
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+              Description of project:
+            </label>
+            <br />
+            <div className="mt-2">
+              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <textarea
+                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  placeholder="...."
+                  {...register("Description", {
+                    required: true,
+                    maxLength: 200,
+                    minLength: 30,
+                  })}
+                />
+              </div>
+            </div>
+            <div>
               {errors.Description?.type === "required" && (
-                <p role="alert">Description is required</p>
+                <p
+                  className="pt-2.5 text-sm font-medium leading-6 text-red-600"
+                  role="alert"
+                >
+                  Description is required
+                </p>
               )}
               {errors.Description?.type === "maxLength" && (
-                <p role="alert">Description must be under 200 characters</p>
+                <p
+                  className="pt-2.5 text-sm font-medium leading-6 text-red-600"
+                  role="alert"
+                >
+                  Description must be under 200 characters
+                </p>
               )}
               {errors.Description?.type === "minLength" && (
-                <p role="alert">Description must be over 30 characters</p>
+                <p
+                  className="pt-2.5 text-sm font-medium leading-6 text-red-600"
+                  role="alert"
+                >
+                  Description must be over 30 characters
+                </p>
               )}
             </div>
-          </div>
-          <br />
-          <label className="block text-sm font-medium leading-6 text-gray-900">
-            Starting Date:
-          </label>
-          <br />
-          <div className="mt-2">
-            {/* <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"> */}
+
+            <br />
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+              Starting Date:
+            </label>
+            <br />
+            <div className="mt-2">
               <input type="date" {...register("Date", { required: true })} />
 
               {errors.Date?.type === "required" && (
-                <p role="alert">Date is required</p>
+                <p
+                  className="pt-2.5 text-sm font-medium leading-6 text-red-600"
+                  role="alert"
+                >
+                  Date is required
+                </p>
               )}
             </div>
-          <br />
-          <label className="block text-sm font-medium leading-6 text-gray-900">
-            Status of project:
-          </label>
-          <br />
-          <div className="mt-2">
-            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-              <select
-                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                {...register("Status", { required: true })}
-              >
-                <option value="Complete">Complete</option>
-                <option value="Working on it">Working on it</option>
-                <option value="Not started">Not started</option>
-              </select>
+            <br />
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+              Status of project:
+            </label>
+            <br />
+            <div className="mt-2">
+              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <select
+                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  {...register("Status", { required: true })}
+                >
+                  <option value="Complete">Complete</option>
+                  <option value="Working on it">Working on it</option>
+                  <option value="Not started">Not started</option>
+                </select>
+              </div>
+            </div>
+            <div>
               {errors.Status?.type === "required" && (
-                <p role="alert">Status is required</p>
+                <p
+                  className="pt-2.5 text-sm font-medium leading-6 text-red-600"
+                  role="alert"
+                >
+                  Status is required
+                </p>
               )}
             </div>
-          </div>
-          <br />
-          <label className="block text-sm font-medium leading-6 text-gray-900">
-            Priority level
-          </label>
-          <br />
-          <div className="mt-2">
-            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-              <select
-                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                {...register("Priority", { required: true })}
-              >
-                <option value="High">High</option>
-                <option value="Medium">Medium</option>
-                <option value="Low">Low</option>
-              </select>
+
+            <br />
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+              Priority level
+            </label>
+            <br />
+            <div className="mt-2">
+              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <select
+                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  {...register("Priority", { required: true })}
+                >
+                  <option value="High">High</option>
+                  <option value="Medium">Medium</option>
+                  <option value="Low">Low</option>
+                </select>
+              </div>
+            </div>
+            <div>
               {errors.Priority?.type === "required" && (
-                <p role="alert">Priority field is required</p>
+                <p
+                  className="pt-2.5 text-sm font-medium leading-6 text-red-600"
+                  role="alert"
+                >
+                  Priority field is required
+                </p>
               )}
             </div>
+
+            <br />
+
+            <button
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              type="submit"
+              disabled={hasPosted}
+            >
+              Submit
+            </button>
+
+            <br />
           </div>
-          <br />
-
-          <button
-            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            type="submit"
-            disabled={hasPosted}
-          >
-            Submit
-          </button>
-
-          <br />
         </div>
-      </div>
       </div>
     </form>
   );
